c5aa4e1204dbcef23e92f6e8fdc54e2b22604123
Navneetcharpe <charpenavneet@gmail.com>
2026-07-24 19:12:59 +0530

    https://github.com/Navneetcharpe/Python-DSA-Journey-Daily

---
+++ for.py
@@ -0,0 +1,18 @@
+a = [1,2,3,4,5]
+sum=0
+for i in a:
+    sum =sum+i
+print(i)
+
+product=1
+for i in a:
+    product=product*i
+print(product)
+
+
+for i in a:
+    for j in a:
+        print("({},{})".format(i,j))
+
+
+
